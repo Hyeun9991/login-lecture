2b4f7721ea987056c697704e5838c1e68f631417
Hyeun9991 <hyeun9991@gmail.com>
2022-06-24 20:47:34 +0900

    프런트 단 javascript 파일 만들기

--- app/app.js
+++ app/app.js
@@ -10,6 +10,7 @@ const home = require('./src/routes/home');
 // 앱 세팅
 app.set('views', './src/views');
 app.set('view engine', 'ejs'); // 엔진 선택
+app.use(express.static(`${__dirname}/src/public`)); // 정적경로로 추가
 
 // use: 미들웨어를 등록해주는 메서드
 app.use('/', home);
